3a9da836dcf92fd820a29b26cd978c238900b426
Markiian Potiuk <markiian250906@gmail.com>
2026-06-17 21:19:42 +0300

    connection fix

--- booking_service/database.py
+++ booking_service/database.py
@@ -6,11 +6,11 @@ import os
 # 1. Рядок підключення до БД
 #    Формат: "postgresql://USER:PASSWORD@HOST:PORT/DB_NAME"
 #    Ці дані ми взяли з вашого docker-compose.yml
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/booking_db")
+DATABASE_URL = "postgresql://user:password@localhost:5432/booking_db"
 
 # 2. Створення "рушія" (engine)
 #    Це головний об'єкт SQLAlchemy для роботи з БД
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 # 3. Створення "фабрики" сесій
 #    Сесія - це ваш "посередник" для спілкування з БД
