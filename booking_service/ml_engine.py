ff169da65638ae7bbced2b5cae136db3466ead4e
JustyM4rk <markiian250906@gmail.com>
2026-06-17 20:06:16 +0300

    Fix import statement for models in ml_engine.py

--- booking_service/ml_engine.py
+++ booking_service/ml_engine.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from database import SessionLocal, engine
-from import models
+import models
 import pickle # Для збереження навченої моделі
 import os
 
